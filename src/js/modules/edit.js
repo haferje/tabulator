55bc247eb7a017c4f3c6ba1810b2fec10644aaa4
Jason Hafer <haferje@gmail.com>
2020-03-18 14:01:32 -0400

    Fix Shift+Tab navigation for Number and Range editors
    The 'number' and 'range' cell editors do not follow the convention when using Shift+Tab to move to the previous cell.  Instead, focus remains in the cell.  Removing the onChange keyhandler for the Shift (keycode 9) key allows it to work as expected.

--- src/js/modules/edit.js
+++ src/js/modules/edit.js
@@ -528,7 +528,6 @@ Edit.prototype.editors = {
 		input.addEventListener("keydown", function(e){
 			switch(e.keyCode){
 				case 13:
-				// case 9:
 				onChange();
 				break;
 
@@ -617,7 +616,6 @@ Edit.prototype.editors = {
 		input.addEventListener("keydown", function(e){
 			switch(e.keyCode){
 				case 13:
-				case 9:
 				onChange();
 				break;
 
